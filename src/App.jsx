f8e30c82125c83bde7152326ff21072c01afaa6e
Jakkrit Kittiwiriya <jakkrit@MacBook-Air-khxng-Jakkrit.local>
2026-04-21 15:35:22 +0700

    add files components

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,26 @@
-// export default function App() {
-//   return (
-//     <div className="min-h-screen flex justify-center bg-blue-950">
-//       <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
-//         <h1 className="w-full p-6 bg-amber-100 font-extrabold">
-//           React App Starter
-//         </h1>
-//         <section className="w-full p-5 bg-amber-100 flex">
-//           <ul className="list-inside list-disc flex-1">
-//             <span className="font-semibold">Tech Stack:</span>
-//             <li>Vite</li>
-//             <li>React</li>
-//             <li>JavaScript</li>
-//             <li>Tailwind</li>
-//           </ul>
-//         </section>
-//       </div>
-//     </div>
-//   );
-// }
-// ================================================== //
-
-
-import Castle from "./components/Castle";
+import { useState } from "react";
+import Castle from "./components/01_Castle";
 
 export default function App() {
+  // Creating State Variable
+  const [question, setQuestion] = useState("");
+
+  const handleQuestion = (e) => {
+    console.log(e);
+    setQuestion(e.traget.value);
+  };
+
   return (
     <div className="pb-80 py-10 gap-y-4 flex flex-col justify-center items-center min-h-screen bg-gray-800 text-white">
-      <p className="text-purple-300">Message for JSD12:
+      <p className="text-purple-300">
+        Message for JSD12:
         <span className="text-yellow-300">
           {/* question or waitng for a message */}
         </span>
       </p>
-      <textarea 
-        value="banana"
-        onChange=""
+      <textarea
+        value={question}
+        onChange={handleQuestion}
         className="bg-white text-black rounded px-2 py-1"
         placeholder="Type your message here..."
       />
@@ -45,5 +32,5 @@ export default function App() {
       </p>
       <Castle />
     </div>
-    );
+  );
 }
