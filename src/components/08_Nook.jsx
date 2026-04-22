75dafad76d6281d06b16071cc93761b19138d8b8
Jakkrit Kittiwiriya <jakkrit@MacBook-Air-khxng-Jakkrit.local>
2026-04-22 14:11:25 +0700

    edit complete day 2

--- src/components/08_Nook.jsx
+++ src/components/08_Nook.jsx
@@ -1,11 +1,15 @@
-import SecretRoom from './09_SecretRoom';
+import SecretRoom from "./09_SecretRoom";
 
-export default function Nook({question, answer, handleAnswer}) {
+export default function Nook({ question, answer, handleAnswer }) {
   return (
     <div className="flex flex-col justify-center items-center pt-10 bg-yellow-200 w-[90%]">
       <h1>Nook</h1>
       {question}
-      <SecretRoom question={question} answer={answer} handleAnswer={handleAnswer}/>
+      <SecretRoom
+        question={question}
+        answer={answer}
+        handleAnswer={handleAnswer}
+      />
     </div>
   );
 }
